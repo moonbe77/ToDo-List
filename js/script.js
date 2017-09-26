fa7c963ec050496511192080f8aef8fd882352b1
Berny <moonbe77@gmail.com>
2017-09-25 21:58:47 -0300

    creando la funcion para editar

--- js/script.js
+++ js/script.js
@@ -7,8 +7,13 @@
 */
 var form = document.getElementById("formCreate")
 var showTask = document.getElementById("showTask")
+var eliminar = document.getElementsByClassName("eliminar")
+var editar = document.getElementsByClassName("editar")
+var realizado = document.getElementsByClassName("realizado")
+var modal = document.getElementById("modal")
 var tareas = []// no es necesario
 var obTarea ={}
+
 //objeto tarea
 /*var objetoTarea = {
     id: 0,
@@ -16,7 +21,7 @@ var obTarea ={}
     desc: "algo"
 }*/
 
-function crearTarea(titulo,desc){
+function crearTarea(titulo,desc){ //constructor del objeto tarea
     this.id="td"+Date.now()  //metodo para generar el id unico
     this.titulo= titulo
     this.desc=desc
@@ -37,7 +42,7 @@ var almacenar = function (id,tarea) {
     mostrarTareas(tarea.id,tarea.titulo,tarea.desc)
 }
 
-var recuperarTareas = function (){ //busco las tareas en LS y las muestro usando la funcion mostrarTareas()
+var recuperarTareas = function (){ //busco las tareas en LocalStorage y las muestro usando la funcion mostrarTareas()
     for (x=0; x<=localStorage.length-1; x++)  {  
         clave = localStorage.key(x); 
         //console.log(clave)
@@ -48,20 +53,70 @@ var recuperarTareas = function (){ //busco las tareas en LS y las muestro usando
             mostrarTareas(tarea.id,tarea.titulo,tarea.desc)         
         }
       }
-
+      crearEvento()
 }
 
 var mostrarTareas= function (id,titulo,desc){
-    
-var newBox = document.createElement("div")
-var contenido = '<div class="box"><div class="id">Id: '+id+'</div><div class="titulo"><b> Titulo:'+titulo+'</b></div><div class="descr">Tarea: '+desc+'</div><nav class="level is-mobile"><div class="level-left"><a class="level-item" onclick="removeTask('+id+')" title="Eliminar Tarea"><span class="icon is-small"><i class="fa fa-trash-o"></i></span></a><a class="level-item"><span class="icon is-small"><i class="fa fa-pencil" title="Editar"></i></span>    </a><a class="level-item"><span class="icon is-small"><i class="fa fa-heart"></i></span>    </a></div></nav></div>';
-newBox.innerHTML = contenido
-showTask.appendChild(newBox)
+    var boxContent = '<div class="box">\
+    <div class="id">Id: '+id+'</div>\
+        <div class="titulo"><b> Titulo:'+titulo+'</b></div>\
+        <div class="descr">Tarea: '+desc+'</div>\
+        <nav class="level is-mobile" ><div class="level-left" id="'+id+'">\
+        <a class="level-item eliminar" title="Eliminar Tarea" dataID="'+id+'" dataID="'+id+'">\
+        <span class="icon is-small"><i class="fa fa-trash-o"></i></span></a>\
+        <a class="level-item editar" dataID="'+id+'"><span class="icon is-small"><i class="fa fa-pencil" title="Editar"></i></span></a>\
+        <a class="level-item realizado" dataID="'+id+'"><span class="icon is-small"><i class="fa fa-heart"></i></span></a>\
+        </div>\
+        </nav>\
+    </div>';    
+    var newBox = document.createElement("div")
+    newBox.innerHTML = boxContent
+    showTask.appendChild(newBox)
+    crearEvento()    
 }
 
-var removeTask = function(id){
-    localStorage.removeItem(id);
-    console.log("Removiendo Tarea id= ",id)
+var removeTask = function(event){
+    localStorage.removeItem(event.currentTarget.parentNode.id);
+    console.log("Removiendo Tarea id=", event.currentTarget.parentNode.id)
     showTask.innerHTML = ""
-    recuperarTareas()
+    recuperarTareas()    
+}
+
+var editarTask = function(){
+    console.log("Editando=", event.currentTarget.parentNode.id)
+    //localStorage.removeItem(event.currentTarget.parentNode.id);
+    
+    var modalTitulo = document.getElementById("modalId").placeholder = event.currentTarget.parentNode.id    
+    var modalTitulo = document.getElementById("modalTitulo").placeholder = "Test"
+    var modalDesc = document.getElementById("modalDesc").placeholder = "test"
+
+    mostrarModal()             
+}
+
+var realizarTask = function(){
+    //localStorage.removeItem(event.currentTarget.parentNode.id);
+    console.log("realizado=", event.currentTarget.parentNode.id)
+    showTask.innerHTML = ""
+    recuperarTareas()  
+}
+
+function crearEvento(){
+    for (var i = 0; i < eliminar.length; i++) {
+        eliminar[i].addEventListener('click', removeTask)
+        editar[i].addEventListener('click', editarTask)
+        realizado[i].addEventListener('click', realizarTask)
+    } 
+}
+
+var mostrarModal= function(){    
+    modal.classList.toggle("is-active") //con toggle lo que hace es agregar o quitar la clase si ya fue agregada    
+}
+
+var editarTarea = function (params) {
+    event.preventDefault()
+    var titulo = form[0].value
+    var desc = form[1].value
+        obTarea = new crearTarea(titulo,desc)
+        almacenar(obTarea.id,obTarea)
+    console.log(obTarea)
 }
